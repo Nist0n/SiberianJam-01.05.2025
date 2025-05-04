e6cb08f6059d3ca220d55037e712667d2d1069cf
Niston <vadimpavlov@bk.ru>
2025-05-04 19:20:51 +0300

    Fix

---
+++ Assets/RAbbitHuesos.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class RAbbitHuesos : MonoBehaviour
+{
+    private void Update()
+    {
+        this.gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+}
